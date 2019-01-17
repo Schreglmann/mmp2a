54279b2cffdb0959dfbabe452b76543e016ed7c0
Georg Christian Schreglmann <georgschreglmann@gmx.at>
2019-01-17 19:28:04 +0100

    debugging

--- api.php
+++ api.php
@@ -19,13 +19,13 @@ if(isset($_POST["data"])){
         );
         $re = new ACRCloudRecognizer($config);
 
-        $content = file_get_contents("test.mp3");
-        var_dump($_POST);
+        //$content = file_get_contents("test.mp3");
+        //var_dump($_POST);
         $ausgabe="";
-        $ausgabe = $re->recognizeByFileBuffer($content, 0, 10);
+        //$ausgabe = $re->recognizeByFileBuffer($content, 0, 10);
         //$ausgabe = $ausgabe."\n";
-        //$ausgabe = $ausgabe.$re->recognizeByFileBuffer($data,0,10);
+        $ausgabe = $ausgabe.$re->recognizeByFileBuffer($data,0,10);
 
-        echo $ausgabe;
-        //echo json_encode($ausgabe);
+        //echo $ausgabe;
+        echo json_encode($ausgabe);
 ?>
