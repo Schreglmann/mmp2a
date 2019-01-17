35417b2ce3e4c62c26b78fb0952dd2d4dcee5bbd
Georg Christian Schreglmann <georgschreglmann@gmx.at>
2019-01-17 17:28:49 +0100

    Key added

--- test.php
+++ test.php
@@ -3,9 +3,9 @@
 
     // Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
     $config = array(
-        'host' => 'XXX',
-        'access_key' => 'XXX',
-        'access_secret' => 'XXX',
+        'host' => 'identify-eu-west-1.acrcloud.com',
+        'access_key' => 'a96b884113ffeb58429cece86edbf129',
+        'access_secret' => 'isbKQI0JAW0as2udufahHwgU1V8QNxsb05JTeaPz',
         'recognize_type' => ACRCloudRecognizeType::ACR_OPT_REC_AUDIO // ACR_OPT_REC_AUDIO/ACR_OPT_REC_HUMMING/ACR_OPT_REC_BOTH
     );
     $re = new ACRCloudRecognizer($config);
